cff64f9c9087ba9545c6375ade8e3d2fe862de81
fransisko123 <fransisko21200saverio@gmail.com>
2023-01-22 10:18:36 +0800

    finish styling and CRUD students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ApplicationRecord
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :date_of_birth, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+20.times do
+  students = Student.new(
+    name: Faker::Name.unique.name,
+    gender: Faker::Gender.binary_type,
+    date_of_birth: Faker::Date.between(from: '1998-01-01', to: '2002-12-31')
+  )
+  students.save
+end
